06eb0ade6544c1b275395dd04b243f8ad76983af
Fredrik Johansson <fredrik.johansson@gmail.com>
2015-12-27 23:36:28 +0100

    tweak example.c

--- example.c
+++ example.c
@@ -4,7 +4,11 @@ int main()
 {
     double complex z;
 
-    z = ac_hyp2f1(1.3 + 2.1*I, 2.5-0.6*I, 2.0, 3.0+4.0*I);
+    z = ac_erfc(20.0 + 10.0*I);
 
-    printf("%.15g + %.15g\n", creal(z), cimag(z));
+    printf("%.15g + %.15g*I\n", creal(z), cimag(z));
+
+    z = ac_hyp2f1(1.3 - 2.1*I, 2.5 + 0.6*I, 2.0, 3.0 + 4.0*I);
+
+    printf("%.15g + %.15g*I\n", creal(z), cimag(z));
 }
